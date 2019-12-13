f856ec08a234b218bb584b976cce5673830c0427
aman47444 <aman47444>
2019-12-13 13:22:37 +0530

    add login aa

--- src/pages/navbar/Drawer.js
+++ src/pages/navbar/Drawer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 import './style.scss';
 const Drawer = props => {
     let drawerClasses = ['drawer-side'];
@@ -8,7 +9,7 @@ const Drawer = props => {
     return (
         <nav className={drawerClasses}>
             <ul>
-                <li><a href="#">section a</a></li>
+                <li><Link to="/login">Login</Link></li>
                 <li><a href="#">section a</a></li>
                 <li><a href="#">section a</a></li>
             </ul>
